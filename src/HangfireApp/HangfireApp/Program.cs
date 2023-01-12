83253639f8da4efd991b73ae3a0006f6df76c24b
Alexey Orlov <aleksei.orlov@simbirsoft.com>
2023-01-12 12:58:02 +0300

    Added HangfireApp

---
+++ src/HangfireApp/HangfireApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Hangfire;
+using Hangfire.SqlServer;
+
+namespace HangfireApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseColouredConsoleLogProvider()
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage("Server=(localdb)\\MSSQLLocalDB; Database=Hangfire.Sample; Integrated Security=True;", new SqlServerStorageOptions());
+
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Hello, world!"), Cron.Minutely);
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
