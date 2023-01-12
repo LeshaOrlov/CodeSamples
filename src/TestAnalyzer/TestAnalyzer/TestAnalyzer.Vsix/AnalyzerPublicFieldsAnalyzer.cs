998c721aa533538c54d6b8debfc09e629aefc4dd
Alexey Orlov <aleksei.orlov@simbirsoft.com>
2023-01-12 12:58:59 +0300

    Added TestAnalyzerApp

---
+++ src/TestAnalyzer/TestAnalyzer/TestAnalyzer.Vsix/AnalyzerPublicFieldsAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAnalyzer.Vsix
+{
+    internal class AnalyzerPublicFieldsAnalyzer
+    {
+        public const string DiagnosticId = "PublicField";
+        private const string Title = "Filed is public";
+        private const string MessageFormat = "Field '{0}' is public";
+        private const string Category = "Syntax";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(Rule);
+            }
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var fieldSymbol = context.Symbol as IFieldSymbol;
+
+            if (fieldSymbol != null && fieldSymbol.DeclaredAccessibility == Accessibility.Public
+                && !fieldSymbol.IsConst && !fieldSymbol.IsAbstract && !fieldSymbol.IsStatic
+                && !fieldSymbol.IsVirtual && !fieldSymbol.IsOverride && !fieldSymbol.IsReadOnly
+                && !fieldSymbol.IsSealed && !fieldSymbol.IsExtern)
+            {
+                var diagnostic = Diagnostic.Create(Rule, fieldSymbol.Locations[0], fieldSymbol.Name);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
